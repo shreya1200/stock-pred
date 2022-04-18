dcccd3df5873ecbb2c8e10434f396ab03f351a53
Shreya_12 <shreya.gore2000@gmail.com>
2022-04-18 16:11:14 +0530

    Backend updated

--- backend/stocks/views.py
+++ backend/stocks/views.py
@@ -12,11 +12,11 @@ from stocks.prediction import *
 import schedule
 
 # Create your views here.
-  
+
+
 class StocksView(APIView):
     # fetch_data("InfosysFinal.csv")
     # stock_data={"csv_name":{"TataFinal.csv","InfosysFinal.csv","HDFCFinal.csv","BajajFinal.csv","AirtelFinal.csv","AdaniFinal.csv"}}
-    
 
     # scrape_news_now()
     # scrape_all_news()
@@ -33,19 +33,18 @@ class StocksView(APIView):
     #     schedule.run_pending()
     #     time.sleep(1)
 
-    
-        # scrape_news(i['csv_name'],i['keyword'])
-        # scrape()
-    
+    # scrape_news(i['csv_name'],i['keyword'])
+    # scrape()
+
     serializer_class = StocksSerializer
-  
+
     def get(self, request):
-        detail = [ {"name": detail.name,"detail": detail.detail} 
-        for detail in Stocks.objects.all()]
+        detail = [{"name": detail.name, "detail": detail.detail}
+                  for detail in Stocks.objects.all()]
         return Response(detail)
-  
-    def post(self, request):  
+
+    def post(self, request):
         serializer = StocksSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
-            return  Response(serializer.data)
+            return Response(serializer.data)
